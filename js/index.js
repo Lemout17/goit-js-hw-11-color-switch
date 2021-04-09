ae5ff3c3cb4085bb5a26f66db5d5330d84f26861
Vlad Safin <djutsu17@gmail.com>
2021-04-09 20:08:56 +0300

    done

---
+++ js/index.js
@@ -0,0 +1,40 @@
+const colors = [
+  '#FFFFFF',
+  '#2196F3',
+  '#4CAF50',
+  '#FF9800',
+  '#009688',
+  '#795548',
+];
+
+const refs = {
+    start: document.querySelector('[data-action="start"]'),
+    stop: document.querySelector('[data-action="stop"]'),
+    body: document.body,
+}
+refs.stop.disabled = true;
+let interval = null;
+
+function startChangeBgClr() {
+    interval = setInterval(() => {
+        refs.start.disabled = true;
+        refs.stop.disabled = false;
+        refs.start.classList.add('disabled');
+        refs.body.style.backgroundColor = colors[randomIntegerFromInterval(0, colors.length - 1)]
+    },1000)
+}
+
+function stopChangeBgClr() {
+    refs.start.disabled = false;
+    refs.start.classList.remove('disabled');
+    
+    clearInterval(interval);
+}
+
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+refs.start.addEventListener('click', startChangeBgClr);
+refs.stop.addEventListener('click', stopChangeBgClr);
+
